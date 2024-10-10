a92b6ffb35be1ce3c42d945d4585a8f28e736811
Bruno Gomes Martins Alves <brunoalves@estudante.ufscar.br>
2024-10-10 17:13:07 -0300

    Things I left behind

--- milestone_project/final_check.py
+++ milestone_project/final_check.py
@@ -1,7 +1,7 @@
 from continue_choice import continue_choice
 
 def final_check(play_options,player):
-    control = 0
+    won_control = 0
     first_line = (play_options[1] == play_options[2] == play_options[3] != ' ')
     second_line = (play_options[4] == play_options[5] == play_options[6] != ' ')
     third_line = (play_options[7] == play_options[8] == play_options[9] != ' ')
@@ -11,17 +11,12 @@ def final_check(play_options,player):
     first_diagonal = (play_options[1] == play_options[5] == play_options[9] != ' ')
     second_diagonal = (play_options[3] == play_options[5] == play_options[7] != ' ')
 
-    for element in play_options:
-        if element == ' ':
-            control = 0
-            pass
-        else:
-            control = 1
     if (first_line or second_line or third_line or first_column or second_column or third_column or first_diagonal or second_diagonal) == True:
         print(f'Congratulations! Player {player+1} won the game!')
         continue_play = continue_choice()
         play_options = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-    elif control == 1:
+        won_control = 1
+    elif play_options[1] != ' ' and play_options[2] != ' ' and play_options[3] != ' ' and play_options[4] != ' ' and play_options[5] != ' ' and play_options[6] != ' ' and play_options[7] != ' ' and play_options[8] != ' ' and play_options[9] != ' ' and won_control == 0:
         print('It is a draw!')
         continue_play = continue_choice()
         play_options = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
--- modules_packages/name_and_main/one.py
+++ modules_packages/name_and_main/one.py
@@ -3,9 +3,6 @@
 def func():
     print("Func() in one.py")
 
-print("Top level in one.py")
 
 if __name__ == "__main__":
-    print('ONE.PY IS BEING RUN DIRECTLY!')
-else:
-    print("ONE.PY has been imported!")
+    pass
